58c3f0416553e8bf06be4bb5f460efe1d3f4e206
xuecj <xuecjpersonal@163.com>
2023-04-05 00:28:58 +0800

    引入swagger2，创建swagger

---
+++ src/main/java/edu/jayhsueh/emos/wx/config/SwaggerConfig.java
@@ -0,0 +1,62 @@
+package edu.jayhsueh.emos.wx.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xuecj
+ * @version 1.0
+ * @date 2023/4/5 0:10
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("EMOS办公");
+        ApiInfo info = builder.build();
+        docket.apiInfo(info);
+        //设置哪些类添加到swagger
+        ApiSelectorBuilder selectorBuilder = docket.select();
+        selectorBuilder.paths(PathSelectors.any());
+        selectorBuilder.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class));
+        docket = selectorBuilder.build();
+        //上传令牌字符串,在请求头
+        ApiKey apiKey = new ApiKey("token", "token", "header");
+        List<ApiKey> apiKeyList = new ArrayList<>();
+        apiKeyList.add(apiKey);
+        docket.securitySchemes(apiKeyList);
+
+        //设定令牌作用域
+        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] scopes = {scope};
+        SecurityReference reference = new SecurityReference("token", scopes);
+        List refList = new ArrayList();
+        refList.add(reference);
+        SecurityContext context = SecurityContext.builder().securityReferences(refList).build();
+        List cxtList = new ArrayList();
+        cxtList.add(context);
+        docket.securityContexts(cxtList);
+        return docket;
+    }
+
+
+}
